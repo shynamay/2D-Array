1021511f765130dabcd3d5658ee25c2fe9cdfb01
Sheena Mae Pader <111573118+shynamay@users.noreply.github.com>
2022-12-12 18:36:54 +0800

    Add files via upload

---
+++ Array5.java
@@ -0,0 +1,28 @@
+public class Array5 {
+    public int max(int [] array) {
+        int max = 0;
+       
+        for(int i=0; i<array.length; i++ ) {
+           if(array[i]>max) {
+              max = array[i];
+           }
+        }
+        return max;
+
+    }
+
+    public static void main(String[] args) {
+        
+        Array5 m = new Array5();
+        System.out.println("Test Case 1");
+        int arr1_3[] = {1,2,9,3,4};
+        System.out.println("Maximum value in the array is::"+m.max(arr1_3));
+        System.out.println("Test Case 2");
+        int arr2_3[] = {1,2,4,4,-9};
+        System.out.println("Maximum value in the array is::"+m.max(arr2_3));
+        System.out.println("Test Case 3");
+        int arr3_3[] = {-1,0,3,-4-5};
+        System.out.println("Maximum value in the array is::"+m.max(arr3_3));
+        
+    }
+}
