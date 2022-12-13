b51484ade3008d6dbe1887e9992e4519a2b75638
Sheena Mae Pader <111573118+shynamay@users.noreply.github.com>
2022-12-13 22:06:14 +0800

    Add files via upload

---
+++ Array9.java
@@ -0,0 +1,27 @@
+public class Array9
+    {
+    public static void main(String[] args) {
+        
+        //9
+        System.out.println("Given 2 int arrays, each length 2, return a new array length 4 containing all elements. ");
+        System.out.println(" ");
+
+        System.out.println("Test Case 1");
+        int [] arr1_0 = {1, 2};
+        int [] arr1_1 = {3, 4};
+        System.out.println("Output:: " + arr1_0[0] + arr1_0[1] + arr1_1[0] + arr1_1[1]);
+
+        System.out.println(" ");
+        System.out.println("Test Case 2");
+        int [] arr2_0 = {4, 4};
+        int [] arr2_1 = {2, 2};
+        System.out.println("Ouput:: "+ arr2_0[0] + arr2_0[1] + arr2_1[0] + arr2_1[1]);
+
+        System.out.println(" ");
+        System.out.println("Test Case 3");
+        int [] arr3_0 = {9, 2};
+        int [] arr3_1 = {3, 4};
+        System.out.println("Output::" + arr3_0[0] + arr3_0[1] + arr3_1[0] + arr3_1[1]);
+
+        }
+    }     
